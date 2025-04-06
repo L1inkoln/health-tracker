9b278e9db207e7dbd871969b3994aed2cebf6cf0
Artem <domrachev.artem@list.ru>
2025-04-06 05:01:51 +0300

    fix token verify

--- app/auth.py
+++ app/auth.py
@@ -10,8 +10,8 @@ SECRET_KEY = cast(str, os.getenv("JWT_SECRET"))
 
 def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
     try:
-        payload = jwt.decode(credentials.credentials, SECRET_KEY, algorithms=["HS256"])
-        if payload.get("bot_id") != os.getenv("BOT_ID"):
-            raise HTTPException(status_code=403, detail="Invalid bot ID")
+        # Декодируем токен
+        jwt.decode(credentials.credentials, SECRET_KEY, algorithms=["HS256"])
     except jwt.PyJWTError:
+        # Если ошибка декодирования, возвращаем 403
         raise HTTPException(status_code=403, detail="Invalid token")
